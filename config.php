79eaec85f8ba5d4481215dc0c6bb6fa8471fc0ce
anitarazafi <anitarazafindrakotovao@gmail.com>
2022-12-25 01:17:46 +0300

    Added database class

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 return [
     'database' => [
         'host' => 'localhost',
-        'port' => 3306,
-        'dbname' => 'phpnotes',
+        'port' => 1234,
+        'dbname' => 'php',
         'charset' => 'utf8mb4',
     ],
 ];
